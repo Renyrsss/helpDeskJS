972fd403cd87afbe29687e956d27f7f20a8b43eb
Aidar <renys_northside@mail.ru>
2024-02-05 16:00:34 +0600

    deleted checked html

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,5 +1,33 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BahaBaha21 extends Schema.Component {
+  collectionName: 'components_baha_baha21s';
+  info: {
+    displayName: 'baha21';
+    icon: 'calendar';
+  };
+  attributes: {};
+}
+
+export interface BahaBahador extends Schema.Component {
+  collectionName: 'components_baha_bahadors';
+  info: {
+    displayName: 'Bahador';
+    icon: 'calendar';
+  };
+  attributes: {
+    userName: Attribute.String;
+    userQuery: Attribute.String;
+    userPhone: Attribute.String;
+    userComment: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'baha.baha21': BahaBaha21;
+      'baha.bahador': BahaBahador;
+    }
+  }
 }
