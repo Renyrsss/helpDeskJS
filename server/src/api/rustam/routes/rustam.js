fc56b8b7880a8ff662d650f5483466aa860da5f5
Aidar <aidar@MacBook-Pro-Aidar.local>
2024-11-29 15:35:13 +0500

    new rus 2

--- server/src/api/rustam/routes/rustam.js
+++ server/src/api/rustam/routes/rustam.js
@@ -1,18 +1,14 @@
 "use strict";
 
-/**
- * kuat router
- */
-
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
 module.exports = createCoreRouter("api::rustam.rustam", {
   config: {
     find: {
-      middlewares: ["api::rustam.rustam.test-mid"],
+      middlewares: ["api::rustam.test-mid"],
     },
     findOne: {
-      middlewares: ["api::rustam.rustam.test-mid"],
+      middlewares: ["api::rustam.test-mid"],
     },
   },
 });
